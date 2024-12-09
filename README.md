1398ad1914479c6ec9761675319786df8a8c6b5e
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-12-08 22:34:34 -0700

    feat: Adds MQTT support

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ This is a [particle.io](https://www.particle.io/) project that uses the [SI7021 
 * GND <-> D2
 * SCL <-> D1
 * SDA <-> D0
+
+## Development
+
+When developing on a new computer, add a `Secrets.h` file that looks like:
+
+```cpp
+#define MQTT_USER "test"
+#define MQTT_PASS "test"
+```
--- src/si7021.ino
+++ src/si7021.ino
@@ -1,8 +1,19 @@
 // See readme for wiring configuration
 
 #include "Adafruit_Si7021.h"
+#include "MQTT.h"
+#include "Secrets.h"
 
 static Adafruit_Si7021 si7021 = Adafruit_Si7021();
+
+// MQTT
+const char mqttDomain[] = "192.168.4.100";
+const uint16_t mqttPort = 1883;
+char mqttUser[] = MQTT_USER;
+char mqttPass[] = MQTT_PASS;
+MQTT mqttClient(mqttDomain, mqttPort, callback);
+// Do nothing when a message is received
+void callback(char* topic, byte* payload, unsigned int length) {}
 
 // Record last-read and update intervals
 long dataReadTime;
@@ -26,6 +37,8 @@ void setup() {
 
   // Set time intervals
   dataReadTime = Time.now();
+
+  mqttClient.connect(System.deviceID(), mqttUser, mqttPass);
 }
 
 void loop() {
@@ -35,7 +48,14 @@ void loop() {
     // Read SI7021
     temperature = si7021.readTemperature();
     humidity = si7021.readHumidity();
-    
+
+    // Publish to MQTT
+    if (mqttClient.isConnected()) {
+      String mqttDevicePath = "particle/" + System.deviceID() + "/";
+      mqttClient.publish(mqttDevicePath + "temperature", String::format("%f", temperature));
+      mqttClient.publish(mqttDevicePath + "humidity", String::format("%f", humidity));
+    }
+
     dataReadTime = Time.now();
   }
 }
